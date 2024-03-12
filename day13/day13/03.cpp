54cef9e8c2152392669ef6ca7668c7d0ee6dfcf7
hyeily0627 <hj_o5h@naver.com>
2024-03-12 13:59:27 +0900

    Update 03.cpp

--- day13/day13/03.cpp
+++ day13/day13/03.cpp
@@ -1,32 +1,29 @@
 /*
-벡터 객체를 생성하고 정수를 입력 받을때마다 벡터에 삽입하고 평균을 출력
+vector 객체를 생성하고 정수를 입력 받을때마다 벡터에 삽입하고 평균을 출력하시오
 */
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main()
-{
-	vector<int> vec;
-	int input;
-	int sum = 0;
+int main() {
 
-	cout << "정수를 입력하세요" << endl;
-	
-	while (true) 
-	{
-		cout << "정수 입력: ";
-		cin >> input;
+    int b = 0;
+    vector<int> a;
 
-		if (input == 0)
-			break;
-	}
-	vec.push_back(input);
-	for (int i = 0; vec.size(); i++)
-		cout << sum / vec.size() << endl;
-		
-	
+    while (true) {
+        int c;
+        cout << "정수를 입력하세요" << endl;
+        cin >> c;
 
-	return 0;
+        if (!c) break;
+
+        a.push_back(c);
+        for (int i = 0; i < a.size(); i++)
+            cout << a.at(i) << ' ';
+        cout << endl;
+       
+        b += c;
+        cout << " 평균 = " << b / a.size() << endl;
+    }
+    return 0;
 }
